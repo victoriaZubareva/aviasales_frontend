c466e8ffb95573758973758f0571d1a1ff5372f1
Victoria <lion.studio2013@yandex.ru>
2020-04-13 08:07:51 +0300

    Updates

---
+++ precache-manifest.7d7a38788a15c83d0f6970b9fcbd195d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7791a2da0441a2c2e08bbad808743cfd",
+    "url": "/aviasales_frontend/index.html"
+  },
+  {
+    "revision": "187ef7d43934d2beea68",
+    "url": "/aviasales_frontend/static/css/main.e88927b5.chunk.css"
+  },
+  {
+    "revision": "fd103ad03e3b217f6607",
+    "url": "/aviasales_frontend/static/js/2.e3b5d256.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/aviasales_frontend/static/js/2.e3b5d256.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187ef7d43934d2beea68",
+    "url": "/aviasales_frontend/static/js/main.8f1004a1.chunk.js"
+  },
+  {
+    "revision": "88044b12d448a71e2025",
+    "url": "/aviasales_frontend/static/js/runtime-main.e5b5fbd9.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales_frontend/static/media/Logo.0c089234.svg"
+  }
+]);
